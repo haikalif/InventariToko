a9504a5d3a7246aff6ee2dfdc862a1984d183636
Haikal <haikalisratulfajri@gmail.com>
2026-08-20 14:41:38 +0700

    Model purchase order item

--- app/Models/ModelPurchaseOrdersItems.php
+++ app/Models/ModelPurchaseOrdersItems.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\HasCuid;
 use Illuminate\Database\Eloquent\Model;
 
 class ModelPurchaseOrdersItems extends Model
 {
-    
+
+    use HasCuid;
+    Protected $table = 'purchase_orders_items';
+
+    protected $fillable = [
+        'purchase_order_id',
+        'product_id',
+        'jumlah',
+        'jumlah_diterima',
+    ];
+
+    protected $casts = [
+        'subtotal' => 'decimal:2',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function purchaseOrder()
+    {
+        return $this->belongsTo(PurchaseOrder::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
 }
