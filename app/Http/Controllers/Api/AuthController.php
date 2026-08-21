cad5be0b8b9123276f338ae228ec324233170b05
Haikal <haikalisratulfajri@gmail.com>
2026-08-21 10:18:43 +0700

    AuthController -login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,20 +3,48 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\registerRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 
 
 class AuthController extends Controller
 {
-    public function store(registerRequest $request) {
+    public function register(registerRequest $request)
+    {
 
-    $user = User::create($request->validated());
+        $user = User::create($request->validated());
 
-    return response()->json([
+        return response()->json([
             'message' => 'User registered successfully',
             'user' => $user,
         ], 201);
+    }
+
+    public function Login(LoginRequest $request)
+    {
+
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        $expiration = $request->remember_me ? null : now()->addWeeks(1);
+
+        $token = $user->createToken('auth_token' , ['*'], $expiration)->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'access_token' => $token,
+            'user' => $user,
+        ]);
+
     }
 }
