31f73c0705bb749c2135ed167238ecdd33b00051
Haikal <haikalisratulfajri@gmail.com>
2026-08-21 09:50:46 +0700

    AuthController -register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\registerRequest;
+use App\Models\User;
+
 
 
 class AuthController extends Controller
 {
- 
+    public function store(registerRequest $request) {
+
+    $user = User::create($request->validated());
+
+    return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+        ], 201);
+    }
 }
